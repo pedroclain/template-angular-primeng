a4d320ae16e37ab36f978c1d9c3a72b741653481
Pedro Clain <pedroclain20@gmail.com>
2025-08-15 16:54:11 -0300

    Adicionei paginação no componente tabela

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -2,7 +2,7 @@ import { Component, inject } from '@angular/core';
 import { Button } from '../../shared/components/button';
 import { Input } from '../../shared/components/input';
 import { FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
-import { Table, TableColumn } from '../../shared/components/table';
+import { PageChangeEvent, Table, TableColumn } from '../../shared/components/table';
 import { Toast } from '../../shared/components/toast';
 import { ToastService } from '../../shared/services/toast';
 import { Dialog } from '../../shared/components/dialog';
@@ -22,20 +22,19 @@ export class Home {
 
   visible = false;
   control = new FormControl('', [Validators.required]);
-  rows = [
-    {
-      name: 'Pedro Xavier',
-      email: 'email@email.com',
-    },
-    {
-      name: 'Maria Silva',
-      email: 'm@email.com',
-    },
-  ];
   columns: TableColumn[] = [
     { field: 'name', header: 'Name' },
     { field: 'email', header: 'Email' },
   ];
+  allRows = [
+    { name: 'Pedro Xavier', email: 'pedro.xavier@email.com' },
+    { name: 'Maria Souza', email: 'maria.souza@email.com' },
+    { name: 'João Pereira', email: 'joao.pereira@email.com' },
+    { name: 'Ana Lima', email: 'ana.lima@email.com' },
+    { name: 'Carlos Santos', email: 'carlos.santos@email.com' },
+  ];
+
+  rows = this.allRows;
 
   clickSuccess() {
     this.toastService.showSuccess('Sucesso', this.control.value!);
@@ -51,7 +50,14 @@ export class Home {
 
   confirm(event: Event) {
     this.popupService.confirm(event, 'Você confirma essa ação?', () => {
-      this.toastService.showSuccess('Confirmado', 'Ação confirmada com sucesso!');
+      this.toastService.showSuccess(
+        'Confirmado',
+        'Ação confirmada com sucesso!'
+      );
     });
+  }
+
+  pageChange(event: PageChangeEvent) {
+    this.rows = this.allRows.slice(event.page, event.page + event.pageSize);
   }
 }
--- src/app/shared/components/table.ts
+++ src/app/shared/components/table.ts
@@ -1,6 +1,6 @@
 import { CommonModule } from '@angular/common';
-import { Component, input } from '@angular/core';
-import { TableModule } from 'primeng/table';
+import { Component, input, output } from '@angular/core';
+import { TableModule, TablePageEvent } from 'primeng/table';
 
 @Component({
   selector: 'app-table',
@@ -8,9 +8,18 @@ import { TableModule } from 'primeng/table';
   imports: [TableModule, CommonModule],
   template: `
     <p-table
+      [paginator]="true"
+      [rows]="pageSize"
+      [first]="first"
+      lazy="true"
+      [showCurrentPageReport]="true"
+      currentPageReportTemplate="Showing {first} to {last} of {totalRecords} entries"
+      (onPage)="pageChange($event)"
+      [rowsPerPageOptions]="[10, 20, 50]"
       [columns]="columns()"
       [value]="rows()"
       [tableStyle]="{ 'min-width': '50rem' }"
+      [totalRecords]="totalRecords()"
     >
       <ng-template #header let-columns>
         <tr>
@@ -33,9 +42,23 @@ import { TableModule } from 'primeng/table';
 export class Table {
   columns = input<TableColumn[]>([]);
   rows = input<any[]>([]);
-}
+  totalRecords = input<number>(0);
 
+  first = 0;
+  pageSize = 10;
+
+  onPageChange = output<PageChangeEvent>();
+
+  pageChange(event: TablePageEvent) {
+    this.first = event.first;
+    this.onPageChange.emit({ page: event.first, pageSize: event.rows });
+  }
+}
 export interface TableColumn {
   field: string;
   header: string;
+}
+export interface PageChangeEvent {
+  page: number;
+  pageSize: number;
 }
